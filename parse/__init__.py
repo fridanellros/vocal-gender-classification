89bf04b36a5bc1eeeb9e24d79755e264472857d1
Frida Nellros <frida.nellros@gmail.com>
2019-03-25 21:29:11 +0100

    fixed parsing for corner cases

--- parse/__init__.py
+++ parse/__init__.py
@@ -1,5 +1,20 @@
-def paths(folder, sample):
-    return( folder / sample / "wav", folder / sample / "etc" / "README")
+import os
+from pathlib import Path
+
+def paths(folder, sample):    
+    wav_folder = None
+    readme_path = None
+    
+    for root, dirs, files in os.walk(folder / sample):
+        for f in files:
+            if f.lower().endswith("readme"):
+                readme_path = Path(root) / f
+                break
+        for d in dirs:
+            if d.lower().endswith("wav"):
+                wav_folder = Path(root) / d
+    
+    return(wav_folder, readme_path)
 
 def parse_readme(path) -> dict:
     def sanitize(s):
